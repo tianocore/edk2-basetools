tianocore/edk2-basetools

commit 7de218e2bab255adf917e40d7af31412e44ba1c2

BaseTools/Ecc: Fix import issues
1. Complete the full path for import statement. Use "EccMain" to
replace "Ecc" for the absolute path support.
2. Fix some issues on configuration file.
3. Fix an issue of RaiseError not working in EdkLogger.

Contributed-under: TianoCore Contribution Agreement 1.0
Signed-off-by: Hess Chen <hesheng.chen@intel.com>
Reviewed-by: Liming Gao <liming.gao@intel.com>

edk2basetools/Common/EdkLogger.py
@@ -1,7 +1,7 @@
 ## @file
 # This file implements the log mechanism for Python tools.
 #
-# Copyright (c) 2007 - 2015, Intel Corporation. All rights reserved.<BR>
+# Copyright (c) 2007 - 2018, Intel Corporation. All rights reserved.<BR>
 # This program and the accompanying materials
 # are licensed and made available under the terms and conditions of the BSD License
 # which accompanies this distribution.  The full text of the license may be found at
@@ -198,6 +198,7 @@ def error(ToolName, ErrorCode, Message=None, File=None, Line=None, ExtraData=Non
         LogText = _ErrorMessageTemplateWithoutFile % TemplateDict
 
     _ErrorLogger.log(ERROR, LogText)
+    RaiseError = IsRaiseError
     if RaiseError:
         raise FatalError(ErrorCode)
 

edk2basetools/Ecc/CParser.py
@@ -24,8 +24,8 @@ from antlr3.compat import set, frozenset
 #
 ##
 
-from . import CodeFragment
-from . import FileProfile
+from Ecc import CodeFragment
+from Ecc import FileProfile
 
 
 

edk2basetools/Ecc/Check.py
@@ -15,10 +15,10 @@ import Common.LongFilePathOs as os
 import re
 from CommonDataClass.DataClass import *
 import Common.DataType as DT
-from .EccToolError import *
-from .MetaDataParser import ParseHeaderCommentSection
-from . import EccGlobalData
-from . import c
+from Ecc.EccToolError import *
+from Ecc.MetaDataParser import ParseHeaderCommentSection
+from Ecc import EccGlobalData
+from Ecc import c
 from Common.LongFilePathSupport import OpenLongFilePath as open
 from Common.MultipleWorkspace import MultipleWorkspace as mws
 

edk2basetools/Ecc/CodeFragmentCollector.py
@@ -23,13 +23,13 @@ import Common.LongFilePathOs as os
 import sys
 
 import antlr3
-from .CLexer import CLexer
-from .CParser import CParser
+from Ecc.CLexer import CLexer
+from Ecc.CParser import CParser
 
-from . import FileProfile
-from .CodeFragment import Comment
-from .CodeFragment import PP_Directive
-from .ParserWarning import Warning
+from Ecc import FileProfile
+from Ecc.CodeFragment import Comment
+from Ecc.CodeFragment import PP_Directive
+from Ecc.ParserWarning import Warning
 
 
 ##define T_CHAR_SPACE                ' '

edk2basetools/Ecc/Configuration.py
@@ -60,12 +60,14 @@ _ConfigFileToInternalTranslation = {
     "GeneralCheckIndentation":"GeneralCheckIndentation",
     "GeneralCheckIndentationWidth":"GeneralCheckIndentationWidth",
     "GeneralCheckLine":"GeneralCheckLine",
+    "GeneralCheckLineEnding":"GeneralCheckLineEnding",
     "GeneralCheckLineWidth":"GeneralCheckLineWidth",
     "GeneralCheckNoProgma":"GeneralCheckNoProgma",
     "GeneralCheckNoTab":"GeneralCheckNoTab",
     "GeneralCheckNo_Asm":"GeneralCheckNo_Asm",
     "GeneralCheckNonAcsii":"GeneralCheckNonAcsii",
     "GeneralCheckTabWidth":"GeneralCheckTabWidth",
+    "GeneralCheckTrailingWhiteSpaceLine":"GeneralCheckTrailingWhiteSpaceLine",
     "GeneralCheckUni":"GeneralCheckUni",
     "HeaderCheckAll":"HeaderCheckAll",
     "HeaderCheckCFileCommentLicenseFormat":"HeaderCheckCFileCommentLicenseFormat",

edk2basetools/Ecc/Database.py
@@ -27,9 +27,9 @@ from Table.TableFunction import TableFunction
 from Table.TablePcd import TablePcd
 from Table.TableIdentifier import TableIdentifier
 from Table.TableReport import TableReport
-from .MetaFileWorkspace.MetaFileTable import ModuleTable
-from .MetaFileWorkspace.MetaFileTable import PackageTable
-from .MetaFileWorkspace.MetaFileTable import PlatformTable
+from Ecc.MetaFileWorkspace.MetaFileTable import ModuleTable
+from Ecc.MetaFileWorkspace.MetaFileTable import PackageTable
+from Ecc.MetaFileWorkspace.MetaFileTable import PlatformTable
 from Table.TableFdf import TableFdf
 
 ##

edk2basetools/Ecc/EccMain.py
@@ -17,12 +17,12 @@
 from __future__ import absolute_import
 import Common.LongFilePathOs as os, time, glob, sys
 import Common.EdkLogger as EdkLogger
-from . import Database
-from . import EccGlobalData
-from .MetaDataParser import *
+from Ecc import Database
+from Ecc import EccGlobalData
+from Ecc.MetaDataParser import *
 from optparse import OptionParser
-from .Configuration import Configuration
-from .Check import Check
+from Ecc.Configuration import Configuration
+from Ecc.Check import Check
 import Common.GlobalData as GlobalData
 
 from Common.StringUtils import NormPath
@@ -30,14 +30,14 @@ from Common.BuildVersion import gBUILD_VERSION
 from Common import BuildToolError
 from Common.Misc import PathClass
 from Common.Misc import DirCache
-from .MetaFileWorkspace.MetaFileParser import DscParser
-from .MetaFileWorkspace.MetaFileParser import DecParser
-from .MetaFileWorkspace.MetaFileParser import InfParser
-from .MetaFileWorkspace.MetaFileParser import Fdf
-from .MetaFileWorkspace.MetaFileTable import MetaFileStorage
-from . import c
+from Ecc.MetaFileWorkspace.MetaFileParser import DscParser
+from Ecc.MetaFileWorkspace.MetaFileParser import DecParser
+from Ecc.MetaFileWorkspace.MetaFileParser import InfParser
+from Ecc.MetaFileWorkspace.MetaFileParser import Fdf
+from Ecc.MetaFileWorkspace.MetaFileTable import MetaFileStorage
+from Ecc import c
 import re, string
-from .Exception import *
+from Ecc.Exception import *
 from Common.LongFilePathSupport import OpenLongFilePath as open
 from Common.MultipleWorkspace import MultipleWorkspace as mws
 

edk2basetools/Ecc/Exception.py
@@ -16,7 +16,7 @@
 #
 from __future__ import print_function
 from __future__ import absolute_import
-from .Xml.XmlRoutines import *
+from Ecc.Xml.XmlRoutines import *
 import Common.LongFilePathOs as os
 
 # ExceptionXml to parse Exception Node of XML file

commit 270fe7445bb1e3fd5c8b6e2ce41731f1b5a46067

BaseTools: Fix the RaiseError variable issue caused by 7de218e2bab
The bug is that it cause the RaiseError always be set to TRUE even we
call the function with FALSE parameter.

Cc: Hess Chen <hesheng.chen@intel.com>
Cc: Liming Gao <liming.gao@intel.com>
Contributed-under: TianoCore Contribution Agreement 1.1
Signed-off-by: Yonghong Zhu <yonghong.zhu@intel.com>
Reviewed-by: Hess Chen <hesheng.chen@intel.com>

edk2basetools/Common/EdkLogger.py
@@ -198,8 +198,8 @@ def error(ToolName, ErrorCode, Message=None, File=None, Line=None, ExtraData=Non
         LogText = _ErrorMessageTemplateWithoutFile % TemplateDict
 
     _ErrorLogger.log(ERROR, LogText)
-    RaiseError = IsRaiseError
-    if RaiseError:
+
+    if RaiseError and IsRaiseError:
         raise FatalError(ErrorCode)
 
 # Log information which should be always put out