e36f95335c4b3b749bddaed18de4c4d354fe8889
Matthew Carlson <macarl@microsoft.com>
2020-10-28 15:22:01 -0700

    Added publishing pipeline

--- ConfirmVersionAndTag.py
+++ ConfirmVersionAndTag.py
@@ -1,5 +1,5 @@
 ## @file
-# Quick script to check that the wheel/package created is aligned on a git tag.
+# Quick script to check that the wheel/package created is has a good version
 # Official releases should not be made from non-tagged code.
 #
 # Copyright (c) Microsoft Corporation
@@ -11,6 +11,8 @@ import glob
 import os
 import sys
 
+
+# confirm the tag
 p = os.path.join(os.getcwd(), "dist")
 whl_files = glob.glob(os.path.join(p, "*.whl"))
 if(len(whl_files) == 0):
@@ -21,9 +23,24 @@ if(len(whl_files) != 1):
     raise Exception("Too many wheel files")
 rfn = os.path.relpath(whl_files[0], os.getcwd())
 v = rfn.split("-")[1]
-if v.count(".") != 1:
+if v.count(".") != 2:
     raise Exception("Version %s not in format major.minor.patch" % v)
 if "dev" in v:
     raise Exception("No Dev versions allowed to be published.")
-print("version: " + str(v))
+version = str(v)
+print("version: " + version)
+
+# create a git tag at this point if we are running this on the server
+if "AGENT_ID" in os.environ or "SYSTEM_JOBID" in os.environ:  # check if we're actually running on the server
+    from edk2toollib.utility_functions import RunCmd
+    print("Creating tag and pushing")
+    ret = RunCmd("git", f"tag v{version}")
+    if ret != 0:
+        print("error creating tag")
+        sys.exit(1)
+    ret = RunCmd("git", "push origin --tags")
+    if ret != 0:
+        print("error pushing")
+        sys.exit(1)
+
 sys.exit(0)
--- setup.py
+++ setup.py
@@ -8,13 +8,40 @@
 ##
 
 import setuptools
+import subprocess
+import sys
 from setuptools.command.sdist import sdist
 from setuptools.command.install import install
 from setuptools.command.develop import develop
 
 
-with open("readme.md", "r") as fh:
-    long_description = fh.read()
+# figure out what the current version is
+version_parts = ["0", "1"]
+# query pypi to see what the 
+pip_cmds = [sys.executable, '-m', 'pip', 'search', 'edk2-basetools']
+pypi_results = str(subprocess.check_output(pip_cmds).decode('UTF-8')).strip().splitlines()
+short_version = "0"
+for pypi_package in pypi_results:
+    if not pypi_package.strip().lower().startswith("edk2-basetools "):
+        continue
+    package_parts = pypi_package.split()
+    package_version = package_parts[1].strip("() ")
+    package_ver_parts = package_version.split(".")
+    if len(package_ver_parts) < 2:
+        print(f"The previous version didn't have a second element {package_version}")
+        raise RuntimeError()
+    elif int(package_ver_parts[0]) != int(version_parts[0]) or int(package_ver_parts[1]) != int(version_parts[1]):
+        print("Upgrading the version")
+        short_version = 0
+    elif len(package_ver_parts) != 3:
+        print(f"The previous version didn't have a third element {package_version}")
+    else:
+        short_version = int(package_ver_parts[2]) + 1
+if short_version is None:
+    raise ValueError("Unable to query pypi for the latest version of edk2-basetools")
+version_parts.append(str(short_version))
+version = ".".join(version_parts)
+print(f"Version selected: {version}")
 
 
 class PostSdistCommand(sdist):
@@ -38,6 +65,9 @@ class PostDevCommand(develop):
         develop.run(self)
 
 
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="edk2-basetools",
     version=version,
@@ -55,7 +85,7 @@ setuptools.setup(
         'develop': PostDevCommand,
     },
     include_package_data=True,
-    use_scm_version=True,
+    use_scm_version=False,
     setup_requires=['setuptools_scm'],
     install_requires=[
         'antlr4-python3-runtime'
